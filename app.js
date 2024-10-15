852d3e187db9ecf5f0784ed9bf7fb0760ae16ca0
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 03:16:10 -0700

    task 4 completed

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ let raceNumber = Math.floor(Math.random() * 1000);
 let earlyRegistration = true;
 let runnerAge = 19;
 
+if (earlyRegistration === true && runnerAge > 18) {
+  raceNumber += 1000;
+}
